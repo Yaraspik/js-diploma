9b500aebfda37e0360234d82db812fa126db4363
Stepan Mareev <yaraspik@ya.ru>
2023-09-17 16:59:52 +0300

    Edited GameState

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,41 +1,100 @@
 export default class GameState {
-  constructor() {
-    this.hoveredCell = null; // Нужно только для того, чтобы убирать выделение противника,
-    // когда моего персонажа умертвят
-    this.selectedChar = null;
-    this.step = 'player';
-    this.level = 0;
-    this.blockField = false;
-    this.score = 0; // Не понял про какие очки идет речь
-    this.computerChars = [];
-    this.playerChars = [];
-  }
+  #hoveredCell = null;
 
-  get positionedCharacters() {
-    return this.playerChars.concat(this.computerChars);
-  }
+  #selectedChar = null;
+
+  #step = 'player';
+
+  #level = 0;
+
+  #blockField = false;
+
+  #score = 0; // Не понимаю о каких баллах речь
+
+  #computerChars = [];
+
+  #playerChars = [];
 
   getPositionedCharacterByPosition(position) {
     return this.positionedCharacters.find(({ index }) => index === position);
   }
 
-  passMove(value) {
-    this.step = value;
+  passMove() {
+    if (this.#step === 'player') {
+      this.#step = 'computer';
+      return;
+    }
+
+    this.#step = 'player';
   }
 
   deselect() {
-    this.selectedChar = null;
+    this.#selectedChar = null;
   }
 
-  get(title) {
-    return this[title];
+  select(positionedCharacter) {
+    this.#selectedChar = positionedCharacter;
   }
 
-  set(title, value) {
-    this[title] = value;
+  hover(cell) {
+    this.#hoveredCell = cell;
   }
 
   nextLevel() {
-    this.level += 1;
+    this.#level += 1;
+  }
+
+  startNewGame(playerChars, computerChars) {
+    this.#level = 0;
+    this.#blockField = false;
+    this.#playerChars = playerChars;
+    this.#computerChars = computerChars;
+  }
+
+  loadGameData(res) {
+    this.#blockField = res.blockField;
+    this.#score = res.score;
+    this.#computerChars = res.computerChars;
+    this.#playerChars = res.playerChars;
+    this.#level = res.level;
+    this.#step = res.step;
+    this.#selectedChar = null;
+  }
+
+  addNewEnemies(enemies) {
+    this.#computerChars = enemies;
+  }
+
+  addNewPlayersCharacters(chars) {
+    this.#playerChars = chars;
+  }
+
+  // getters
+  get level() {
+    return this.#level;
+  }
+
+  get playerChars() {
+    return this.#playerChars;
+  }
+
+  get computerChars() {
+    return this.#computerChars;
+  }
+
+  get blockField() {
+    return this.#blockField;
+  }
+
+  get selectedChar() {
+    return this.#selectedChar;
+  }
+
+  get hoveredCell() {
+    return this.#hoveredCell;
+  }
+
+  get positionedCharacters() {
+    return this.#playerChars.concat(this.#computerChars);
   }
 }
